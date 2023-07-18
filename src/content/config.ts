e666d842ab1fd7097a76b8ccd6a8680b0c09dc4b
kirkwat <watson.kirk00@gmail.com>
2023-07-18 02:15:25 -0500

    feat: add projects

--- src/content/config.ts
+++ src/content/config.ts
@@ -24,18 +24,7 @@ const project = defineCollection({
   }),
 });
 
-const showcase = defineCollection({
-  type: "data",
-  schema: z.object({
-    title: z.string().min(1),
-    image: z.string(),
-    url: z.string().url(),
-    featured: z.number().min(1).optional(),
-  }),
-});
-
 export const collections = {
   cert,
   project,
-  showcase,
 };
