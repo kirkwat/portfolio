d910c05b006105493c43c3c9b56ace1443bc2806
kirkwat <watson.kirk00@gmail.com>
2023-11-01 17:23:19 -0500

    build: astro v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   site: "https://kirkwatson.net/",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-  ],
+  integrations: [tailwind(), mdx()],
   vite: {
     ssr: {
       external: ["svgo"],
