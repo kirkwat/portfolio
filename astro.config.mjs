94eb570f07452271256aa1246d7d38dbb2de47b6
kirkwat <watson.kirk00@gmail.com>
2023-11-01 17:43:21 -0500

    ci: vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import vercel from "@astrojs/vercel/static";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://kirkwatson.net/",
   integrations: [tailwind(), mdx()],
-  vite: {
-    ssr: {
-      external: ["svgo"],
-    },
-  },
+  output: "static",
+  adapter: vercel({
+    imageService: true,
+  }),
 });
