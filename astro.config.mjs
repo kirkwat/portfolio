6d724060b6acc4346fbd259d9b0e9d287d03aaff
kirkwat <watson.kirk00@gmail.com>
2023-11-01 18:04:51 -0500

    feat: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://kirkwatson.net/",
-  integrations: [tailwind(), mdx()],
+  site: "https://kirkwatson.net",
+  integrations: [tailwind(), mdx(), sitemap()],
   output: "static",
   adapter: vercel({
     imageService: true,
